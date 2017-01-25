ff6dde0a7371c8d03d5ebd49d70c1c8e41b82fb9
kremlion1 <nthhfgbz@yandex.ru>
2017-01-25 18:03:16 +0500

    removed all test methods. Created class Client with get_html - returned response from any url. method get_games returns list of dictionaries from url. sleep logic must be in another def

--- client/get_games.py
+++ client/get_games.py
@@ -17,57 +17,50 @@ class Profiler(object):
         print "Elapsed time: {:.3f} sec".format(time.time() - self._startTime)
 
 
-root = 'http://www.metacritic.com/'
-SLOW_DOWN = False
+class Client(object):
+    block_date = time.time()
 
-def get_html(url):
-    start_time=time.time()
-    headers = {"User-Agent": "Mozilla/5.001 (windows; U; NT4.0; en-US; rv:1.0) Gecko/25250101"}
-    html = requests.get(url, headers=headers).content.decode('utf-8')
-    wait =12.0-(time.time()-start_time)
-    if (wait>0):
-        sleep(wait)
-    return html
+    def get_html(self, url):
+        if time.time() < self.block_date:
+            sleep(self.block_date-time.time())
+        headers = {"User-Agent": "Mozilla/5.001 (windows; U; NT4.0; en-US; rv:1.0) Gecko/25250101"}
+        html_request = requests.get(url, headers=headers).content.decode('utf-8')
+        self.block_date = time.time() + (300 if "null" == html else 12)
+        return html_request
 
-
-
-def get_pages(genre):
-    def scrape():
-        page_content = get_html(genre)
-        return page_content
+    def get_games(self, html_request):
+        page_content = self.get_html(html_request)
+        #return page_content
         document = json.loads(page_content).values()[0]
-        """
+        cards = []
         match1 = re.findall("<a class=\"market_listing_row_link\" [\s\S]*?</a>", document)
         for m in match1:
-            lpn_text = re.findall("<span class=\"normal_price\">([\s\S]*?)</span>", m)
-            print lpn_text[0]
-        print lpn_text
+            href = re.findall("<a class=\"market_listing_row_link\" href=\"([\s\S]*?)\" id=\"resultlink_0\">", m)[0]
+            img_src = re.findall("<img id=\"result_0_image\" src=\"([\s\S]*?)\" style=\"\"", m)[0]
+            num_listings_qty = re.findall("<span class=\"market_listing_num_listings_qty\">([\s\S]*?)</span>", m)[0]
+            price = re.findall("<span class=\"normal_price\">([\s\S]*?)</span>", m)[0]
+            #item_name = re.findall("class=\"market_listing_item_name\".*>([\s\S]*?)</span><br/>", m)[0]
+            game_name = re.findall("<span class=\"market_listing_game_name\">([\s\S]*?)</span>", m)[0]
+            cards.append(
+                {'href': href, 'img_src': img_src, 'num_listings_qty': num_listings_qty, 'game_name': game_name,
+                 'price': price})
+            print cards
+        return cards
 
-        match = re.search("var g_rgFilterData = (.*);", page_content)
-        js_var = match.group().encode("cp1251")
-        js_var = js_var[js_var.find("{"):len(js_var)-1:]
-        dict =json.loads(js_var)
-        games =[]
-        for d in dict.values()[0].values()[1].values():
-            games.append(d.values()[1])
-        i=0
 
-"""
-    return scrape()
 
 def main():
-    with Profiler() as p:
-        page=""
-        for i in range(0,51):
-            page=get_pages("http://steamcommunity.com/market/search/render/?query=&start="+str(i)+"0&count=10&search_descriptions=0&sort_column=name&sort_dir=asc&appid=753&category_753_Game%5B%5D=any&category_753_item_class%5B%5D=tag_item_class_2&category_753_item_class%5B%5D=tag_item_class_")
-            if page=="null":
-                print i
-                break
-    sleep(1)
-    """dict = json.load(open('genres.json', 'r'))
-    p = Pool(4)
-    p.map(get_pages, [dict[key] for key in dict.keys()])
-    print('Over')"""
+    client = Client()
+    for i in range(0, 51):
+        page = client.get_games("http://steamcommunity.com/market/search/render/?query=&start=" + str(i) + "0&count=10&search_descriptions=0&sort_column=name&sort_dir=asc&appid=753&category_753_Game%5B%5D=any&category_753_item_class%5B%5D=tag_item_class_2&category_753_item_class%5B%5D=tag_item_class_")
+
+        """
+        dict = json.load(open('genres.json', 'r'))
+        p = Pool(4)
+        p.map(get_pages, [dict[key] for key in dict.keys()])
+        print('Over')
+        """
+
 
 if __name__ == "__main__":
     main()
