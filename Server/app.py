a22d6db65b67bcbe71df89661ca2260b2f17b7df
kremlion1 <nthhfgbz@yandex.ru>
2017-01-26 15:37:46 +0500

    Add simple server

---
+++ Server/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import request
+from flask import abort
+from flask import jsonify
+app = Flask(__name__)
+games = ('game1', 'game2')
+
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json:
+        abort(400)
+
+    return jsonify({'game': games[0], 'action': 'get'}), 201
