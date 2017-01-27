c6d309b014131b8da0cacfbdfe603df99bc5acbc
kremlion1 <nthhfgbz@yandex.ru>
2017-01-27 17:45:17 +0500

    Add example of sql request

--- Server/app.py
+++ Server/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from flask import abort
 from flask import jsonify
+import psycopg2
 app = Flask(__name__)
 games = ('game1', 'game2')
 
@@ -11,7 +12,20 @@ def index():
     return "Hello, World!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    connect = psycopg2.connect(database='steam_base', user='test_user', host='localhost', password='test_password')
+    cursor = connect.cursor()
+
+    #cursor.execute("CREATE TABLE tbl(id INT, data JSON);")
+
+    #cursor.execute('INSERT INTO tbl VALUES (1, \'{ "name":"Tester" }\');')
+    #connect.commit()
+
+    cursor.execute("SELECT * FROM tbl;")
+    for row in cursor:
+        print(row)
+
+    connect.close()
+    app.run(port=80)
 
 
 @app.route('/todo/api/v1.0/tasks', methods=['POST'])
