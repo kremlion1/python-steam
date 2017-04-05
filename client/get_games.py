a420be11efde683e9f4ed80e411c09aabce17cb1
habirov <nthhfgbz@yandex.ru>
2017-04-05 12:33:01 +0500

    edit regexp. add proxy, still not working '(

--- client/get_games.py
+++ client/get_games.py
@@ -24,19 +24,22 @@ class Client(object):
         if time.time() < self.block_date:
             sleep(self.block_date - time.time())
         headers = {"User-Agent": "Mozilla/5.001 (windows; U; NT4.0; en-US; rv:1.0) Gecko/25250101"}
-        html_request = requests.get(url, headers=headers).content.decode('utf-8')
+        proxies = {
+            'http': 'http://120.132.52.218:8888'
+        }
+        html_request = requests.get(url, headers=headers, proxies=proxies).content.decode('utf-8')
         return html_request
 
     def get_games(self, html_request):
         page_content = self.get_html(html_request)
-        self.block_date = time.time() + (300 if "null" == page_content else 12)
-        #return page_content
+        self.block_date = time.time() + (300 if "null" == page_content else 15)
+        return page_content
         document = json.loads(page_content).values()[0]
         cards = []
         match1 = re.findall("<a class=\"market_listing_row_link\" [\s\S]*?</a>", document)
         for m in match1:
-            href = re.findall("<a class=\"market_listing_row_link\" href=\"([\s\S]*?)\" id=\"resultlink_0\">", m)[0]
-            img_src = re.findall("<img id=\"result_0_image\" src=\"([\s\S]*?)\" style=\"\"", m)[0]
+            href = re.findall("<a class=\"market_listing_row_link\" href=\"([\s\S]*?)\" id=\"resultlink_\d+\">", m)[0]
+            img_src = re.findall("<img id=\"result_\d+_image\" src=\"([\s\S]*?)\" style=\"\"", m)[0]
             num_listings_qty = re.findall("<span class=\"market_listing_num_listings_qty\">([\s\S]*?)</span>", m)[0]
             price = re.findall("<span class=\"normal_price\">([\s\S]*?)</span>", m)[0]
             item_name = re.findall("class=\"market_listing_item_name\".*>([\s\S]*?)</span>\s*<br/>", m)[0]
@@ -48,10 +51,13 @@ class Client(object):
         return cards
 
     def start(self):
-        for i in range(0, 51):
-            page = self.get_games("http://steamcommunity.com/market/search/render/?query=&start=" + str(
-                i) + "0&count=10&search_descriptions=0&sort_column=name&sort_dir=asc&appid=753&category_753_Game%5B%5D=any&category_753_item_class%5B%5D=tag_item_class_2&category_753_item_class%5B%5D=tag_item_class_")
-
+        for i in range(0, 151):
+            page = self.get_games("http://steamcommunity.com/market/search/render/?query=&start=" + str(i)
+                                  + "0&count=10&search_descriptions=0&sort_column=name&sort_dir=asc&appid=753"
+                                    "&category_753_Game%5B%5D=any&category_753_item_class%5B%5D=tag_item_class_2"
+                                    "&category_753_item_class%5B%5D=tag_item_class_")
+            if page == "null":
+                print i
 
 def main():
     client = Client()
